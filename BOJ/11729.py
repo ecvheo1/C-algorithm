5c8dc134d0a9dc9637b4cdbe08bcd34fbf9036b0
ecvheo1 <78195316+ecvheo1@users.noreply.github.com>
2021-02-14 11:17:26 +0900

    solved 11729

---
+++ BOJ/11729.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def hanoi(n, a, b, c):
+    if (n == 1) :
+        print(f'{a} {c}')
+    
+    else :
+        hanoi(n-1, a, c, b)
+        print(f'{a} {c}')
+        hanoi(n-1, b, a, c)
+
+print(2**n-1)
+hanoi(n, 1, 2, 3)
